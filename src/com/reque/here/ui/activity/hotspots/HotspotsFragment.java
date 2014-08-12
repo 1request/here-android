d710fcc39c66c15453227ed8553e170b91d556eb
suddenlydream <suddenlydream@163.com>
2014-08-12 09:02:01 +0800

    del estimote, use altbeacon

--- src/com/reque/here/ui/activity/hotspots/HotspotsFragment.java
+++ src/com/reque/here/ui/activity/hotspots/HotspotsFragment.java
@@ -5,40 +5,49 @@
  */
 package com.reque.here.ui.activity.hotspots;
 
-import java.util.List;
+import java.util.Collection;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.BeaconManager.MonitoringListener;
-import com.estimote.sdk.Region;
 import com.reque.here.R;
 import com.reque.here.ui.activity.BaseFragment;
 
 /**
  *
  */
-public class HotspotsFragment extends BaseFragment {
+public class HotspotsFragment extends BaseFragment implements BeaconConsumer {
 	private static final String TAG = "HotspotsFragment";
-	private BeaconManager mBeaconManager;
+	private BeaconManager beaconManager;
 	private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
 	private static final String UUID_1 = "74278BDA-B644-4520-8F0C-720EAF059935";
 	private static final String UUID_2 = "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0";
-	private static final Region ALL_ESTIMOTE_BEACONS = new Region("maikelong", UUID_2, null, null);
+	private EditText mLogText;
+
+	// private static final Region ALL_ESTIMOTE_BEACONS = new
+	// Region("maikelong", UUID_2, null, null);
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		Log.d(TAG, "onCreateView");
-		View view = inflater.inflate(R.layout.test_layout, null);
-		TextView test = (TextView) view.findViewById(R.id.test_text);
+		View view = inflater.inflate(R.layout.hotspot_layout, null);
+		EditText test = (EditText) view.findViewById(R.id.test_text);
 		test.setText(TAG);
+		mLogText = test;
 		return view;
 	}
 
@@ -52,13 +61,11 @@ public class HotspotsFragment extends BaseFragment {
 	@Override
 	public void onStart() {
 		super.onStart();
-		startScan();
 	}
 
 	@Override
 	public void onStop() {
 		super.onStop();
-		stopScan();
 	}
 
 	@Override
@@ -68,56 +75,60 @@ public class HotspotsFragment extends BaseFragment {
 	}
 
 	private void initBeacon() {
-		mBeaconManager = new BeaconManager(getActivity());
-		mBeaconManager.setRangingListener(new BeaconManager.RangingListener() {
-			@Override
-			public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
-				Log.d(TAG, "Ranged beacons: " + beacons + " region: " + region);
-			}
-		});
-
-		mBeaconManager.setMonitoringListener(new MonitoringListener() {
-
-			@Override
-			public void onExitedRegion(Region region) {
-				Log.d(TAG, "onExitedRegion region: " + region);
-			}
-
-			@Override
-			public void onEnteredRegion(Region region, List<Beacon> beacons) {
-				Log.d(TAG, "onEnteredRegion region: " + region + " beacons: " + beacons);
-			}
-		});
-	}
-
-	private void startScan() {
-		mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-			@Override
-			public void onServiceReady() {
-				try {
-					Log.d(TAG, "startRanging");
-					mBeaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
-					mBeaconManager.startMonitoring(ALL_ESTIMOTE_BEACONS);
-					// mBeaconManager.startRanging(null);
-					// mBeaconManager.startMonitoring(null);
-				} catch (RemoteException e) {
-					Log.e(TAG, "Cannot start ranging", e);
-				}
-			}
-		});
-	}
-
-	private void stopScan() {
-		try {
-			mBeaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
-			// mBeaconManager.stopMonitoring(null);
-		} catch (RemoteException e) {
-			Log.e(TAG, "Cannot stop but it does not matter now", e);
-		}
+		beaconManager = BeaconManager.getInstanceForApplication(getActivity().getApplicationContext());
+		beaconManager.bind(this);
+		beaconManager.debug = true;
 	}
 
 	private void release() {
 		Log.d(TAG, "onDestroy");
-		mBeaconManager.disconnect();
+		beaconManager.unbind(this);
+	}
+
+	@Override
+	public void onBeaconServiceConnect() {
+		Log.d(TAG, "onBeaconServiceConnect");
+		beaconManager.setRangeNotifier(new RangeNotifier() {
+			@Override
+			public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+				Log.d(TAG, "didRangeBeaconsInRegion beacons: " + beacons + " region: " + region);
+				if (beacons.size() > 0) {
+					Beacon firstBeacon = beacons.iterator().next();
+					logToDisplay("The first beacon " + firstBeacon.toString() + " is about "
+							+ firstBeacon.getDistance() + " meters away.");
+				}
+			}
+
+		});
+
+		try {
+			beaconManager.startRangingBeaconsInRegion(new Region(UUID_2, null, null, null));
+		} catch (RemoteException e) {
+		}
+	}
+
+	private void logToDisplay(final String line) {
+		getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				mLogText.append(line + "\n");
+			}
+		});
+	}
+
+	@Override
+	public Context getApplicationContext() {
+		return getActivity().getApplicationContext();
+	}
+
+	@Override
+	public void unbindService(ServiceConnection connection) {
+		Log.d(TAG, "onBeaconServiceConnect");
+		getActivity().unbindService(connection);
+	}
+
+	@Override
+	public boolean bindService(Intent intent, ServiceConnection connection, int mode) {
+		Log.d(TAG, "onBeaconServiceConnect");
+		return getActivity().bindService(intent, connection, mode);
 	}
 }
